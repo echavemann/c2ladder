388e3eded6eec13aed1320e6f3355868a5055cde
echavemann <echavemann@gmail.com>
2023-07-07 19:17:21 -0700

    800-4

---
+++ 800/4.cpp
@@ -0,0 +1,48 @@
+#pragma GCC optimize("O2,no-stack-protector,unroll-loops,fast-math")
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long
+#define MOD 1000000007
+#define PB push_back
+#define F first
+#define S second
+#define uset unordered_set
+#define umap unordered_map
+#define MP make_pair;
+#define vt vector
+#define all(x) begin(x), end(x)
+#define rall(x) rbegin(x), rend(x)
+#define sz(x) (int)x.size()
+#define REP(i, a) for (int i = 0; i < a; i++)
+#define pii pair<int, int>;
+#define pll pair<ll, ll>;
+
+using namespace std;
+
+typedef vector<ll> vl;
+typedef pair<ll,ll> pl;
+
+void solve()
+{	
+	vt<ll> acc;
+	REP(i, 4) {
+		ll z; cin >> z;
+		acc.PB(z);
+	}
+	if ((max(acc[2], acc[3]) < min(acc[0], acc[1]))||(max(acc[0],acc[1]) < min(acc[3],acc[2]))) cout << "No" << endl;
+	else cout << "Yes" << endl;
+
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+   	ll t;
+    	cin >> t;
+    	while (t--)
+    	{
+        	solve();
+    	}
+    	return 0;
+}
