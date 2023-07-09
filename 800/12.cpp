c227082a2b09bbd4b79bfc7f4bc6becb6ba00165
= <echavemann@gmail.com>
2023-07-09 11:01:26 -0700

    800-12

---
+++ 800/12.cpp
@@ -0,0 +1,55 @@
+#pragma GCC optimize("O2,no-stack-protector,unroll-loops,fast-math")
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long
+#define MOD 1000000007
+#define PB push_back
+#define F first
+#define S second
+#define uset unordered_set
+#define umap unordered_map
+#define MP make_pair;
+#define vt vector
+#define all(x) begin(x), end(x)
+#define rall(x) rbegin(x), rend(x)
+#define sz(x) (int)x.size()
+#define REP(i, a) for (int i = 0; i < a; i++)
+#define pii pair<int, int>;
+#define pll pair<ll, ll>;
+
+using namespace std;
+
+typedef vector<ll> vl;
+typedef pair<ll,ll> pl;
+
+void solve()
+{
+    // your code here
+  //
+
+  ll n; cin >> n;
+  ll res = 0;
+  ll ld = 10;
+  while (n >= 10) {
+    ld = n%10;
+    n/=10;
+    res += 9;
+  }
+  res += n;
+  if (ld < n) res--;
+  cout << res << endl;
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+   	ll t;
+    	cin >> t;
+    	while (t--)
+    	{
+        	solve();
+    	}
+    	return 0;
+}
+
