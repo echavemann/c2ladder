256fde5c00b4f0c33c62fa65709bda4aff50d7dc
= <echavemann@gmail.com>
2023-07-11 08:18:38 -0700

    800-18

---
+++ 800/18.cpp
@@ -0,0 +1,53 @@
+#pragma GCC optimize("O2,no-stack-protector,unroll-loops,fast-math")
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long
+#define MOD 1000000007
+#define PB push_back
+#define F first
+#define S second
+#define uset unordered_set
+#define umap unordered_map
+#define MP make_pair;
+#define vt vector
+#define all(x) begin(x), end(x)
+#define rall(x) rbegin(x), rend(x)
+#define sz(x) (int)x.size()
+#define REP(i, a) for (int i = 0; i < a; i++)
+#define pii pair<int, int>;
+#define pll pair<ll, ll>;
+
+using namespace std;
+
+typedef vector<ll> vl;
+typedef pair<ll,ll> pl;
+
+void solve()
+{
+    // your code here
+    ll n; cin >> n; ll min = 101; ll cnt = 1;
+    REP(i, n) {
+        ll z; cin >> z;
+        if (z < min) {
+            min = z;
+            cnt = 1;
+        }
+        else if (z == min) {
+            cnt++;
+        }
+    }
+    cout << n-cnt << endl;
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+   	ll t;
+    	cin >> t;
+    	while (t--)
+    	{
+        	solve();
+    	}
+    	return 0;
+}
