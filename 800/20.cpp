ab47fd02201e4e6a29df0f6ae93baa32637d9995
= <echavemann@gmail.com>
2023-07-11 08:53:03 -0700

    800-20

---
+++ 800/20.cpp
@@ -0,0 +1,54 @@
+#pragma GCC optimize("O2,no-stack-protector,unroll-loops,fast-math")
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long
+#define MOD 1000000007
+#define PB push_back
+#define F first
+#define S second
+#define uset unordered_set
+#define umap unordered_map
+#define MP make_pair;
+#define vt vector
+#define all(x) begin(x), end(x)
+#define rall(x) rbegin(x), rend(x)
+#define sz(x) (int)x.size()
+#define REP(i, a) for (int i = 0; i < a; i++)
+#define pii pair<int, int>;
+#define pll pair<ll, ll>;
+
+using namespace std;
+
+typedef vector<ll> vl;
+typedef pair<ll,ll> pl;
+
+void solve()
+{
+    // your code here
+    ll x, y, c, a, b; cin >> x >> y >> c;
+    a = max(x,y); b = min(x,y);
+    if ((a - b >= b)&&(2*(a-b) >= c)) {
+        if (c <= a-b) {
+            c += (a-b);
+        }
+        else {
+            c -= (a-b);
+        }
+        cout << c << endl;
+        return;
+    }
+    cout << -1 << endl;
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+   	ll t;
+    	cin >> t;
+    	while (t--)
+    	{
+        	solve();
+    	}
+    	return 0;
+}
