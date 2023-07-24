5b12653ffaf775e48ed42ce090029127049b2f7f
= <echavemann@gmail.com>
2023-07-24 13:55:14 -0700

    800-37

---
+++ 800/37.cpp
@@ -0,0 +1,66 @@
+#pragma GCC optimize("O2,no-stack-protector,unroll-loops,fast-math")
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long
+#define db double
+#define MOD 1000000007
+#define int128 __int128
+#define PB push_back
+#define F first
+#define S second
+#define uset unordered_set
+#define umap unordered_map
+#define MP make_pair
+#define vt vector
+#define ins insert
+#define all(x) begin(x), end(x)
+#define rall(x) rbegin(x), rend(x)
+#define sz(x) (int)x.size()
+#define REP(i, a) for (int i = 0; i < a; i++)
+#define pii pair<int, int>;
+#define setprf(x) setprecision(x) << fixed
+#define setpr(x) setprecision(x)
+#define lb lower_bound
+#define ub upper_bound
+#define pll pair<ll, ll>;
+
+using namespace std;
+
+template<class T> using pq = priority_queue<T>;
+template<class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+
+typedef vector<ll> vl;
+typedef pair<ll,ll> pl;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef complex<ld> cd;
+
+void solve()
+{
+    ll n; cin >> n; 
+    map<ll,ll> freq;
+    ll res = 0;
+    REP(i, n) {
+        ll z; cin >> z;
+        freq[abs(z)]++;
+    }
+    for (auto x : freq) {
+        if (x.F == 0) res++; 
+        else res += min(2ll, x.S);
+    }
+    cout << res << endl;
+    // Be insane or die trying.
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+   	ll t;
+    	cin >> t;
+    	while (t--)
+    	{
+        	solve();
+    	}
+    	return 0;
+}
